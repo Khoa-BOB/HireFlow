4a1f7bc117e1ca58e5e34ef9f9a75f24d5734d0e
NGUYEN ANH KHOA TRAN <trannguyenanhkhoa05122003@gmail.com>
2026-06-27 15:47:23 -0500

    Add roles table

---
+++ migrations/000002_create_roles_table.up.sql
@@ -0,0 +1,14 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+CREATE TABLE roles(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+
+    name VARCHAR(50) NOT NULL UNIQUE,
+    description TEXT,
+    
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT now()
+);
+
+INSERT INTO roles(name, description)
+VALUES ('admin', 'System administration');
